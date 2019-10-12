8192863bebf950250f261aa573e7d85df12ae827
sbx_user1051 <sbx_user1051@169.254.210.189>
2019-10-12 10:03:00 +0000

    Sat Oct 12 10:03:00 UTC 2019

--- src/main/java/befaster/solutions/CHK/Product.java
+++ src/main/java/befaster/solutions/CHK/Product.java
@@ -2,27 +2,39 @@ package befaster.solutions.CHK;
 
 import com.google.common.collect.ImmutableMap;
 
+import java.util.Comparator;
+import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class Product {
 
-    private final char sku;
-    private final int unitPrice;
-    private final SortedMap<Integer, Integer> multiBuyPrices = new TreeMap<>();
+  private final char sku;
+  private final int unitPrice;
+  private final SortedMap<Integer, Integer> multiBuyPrices =
+      new TreeMap<>(Comparator.reverseOrder());
 
-    public Product(char sku, int unitPrice) {
-        this.sku = sku;
-        this.unitPrice = unitPrice;
-    }
+  public Product(char sku, int unitPrice) {
+    this.sku = sku;
+    this.unitPrice = unitPrice;
+  }
 
-    public Product withMultibuyOffer(int units, int price) {
-        multiBuyPrices.put(units, price);
-        return this;
-    }
+  public Product withMultibuyOffer(int units, int price) {
+    multiBuyPrices.put(units, price);
+    return this;
+  }
 
-    public int getTotalPrice(int quantity) {
-        return quantity * unitPrice; //Will come back to this in a minute
+  public int getTotalPrice(int quantity) {
+    int remaining = quantity;
+    int total = 0;
+    for (Map.Entry<Integer, Integer> multibuyEntry : multiBuyPrices.entrySet()) {
+      while (remaining >= multibuyEntry.getKey()) {
+        total += multibuyEntry.getValue();
+        remaining -= multibuyEntry.getKey();
+      }
     }
+    return total + remaining * unitPrice;
+  }
 }
+
 
