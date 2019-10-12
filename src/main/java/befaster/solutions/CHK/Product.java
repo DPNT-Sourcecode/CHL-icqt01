3981357f174f41662dc86fa87835720d297bb8f5
sbx_user1051 <sbx_user1051@169.254.210.189>
2019-10-12 09:57:00 +0000

    Sat Oct 12 09:57:00 UTC 2019

---
+++ src/main/java/befaster/solutions/CHK/Product.java
@@ -0,0 +1,27 @@
+package befaster.solutions.CHK;
+
+import com.google.common.collect.ImmutableMap;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class Product {
+
+    private final char sku;
+    private final int unitPrice;
+    private final SortedMap<Integer, Integer> multiBuyPrices = new TreeMap<>();
+
+    public Product(char sku, int unitPrice) {
+        this.sku = sku;
+        this.unitPrice = unitPrice;
+    }
+
+    public Product withMultibuyOffer(int units, int price) {
+        multiBuyPrices.put(units, price);
+        return this;
+    }
+
+    public int getTotalPrice(int quantity) {
+
+    }
+}
